e3620d7962b348383f3581531b0e332952fbb03a
Pato Cantu <laspatocuentas@gmail.com>
2024-03-15 16:11:08 -0600

    First Commit

---
+++ HistorIA/HistorIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistorIAApp.swift
+//  HistorIA
+//
+//  Created by Patricio Cantu on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HistorIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
