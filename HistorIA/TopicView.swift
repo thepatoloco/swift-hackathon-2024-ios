bb635a24b5c64fcf21e80afd79014bf625a98366
Alumno <alumno@MacBook-Air-de-DNTI.local>
2024-03-16 09:55:25 -0600

    Final Comit

---
+++ HistorIA/TopicView.swift
@@ -0,0 +1,18 @@
+//
+//  TopicView.swift
+//  HistorIA
+//
+//  Created by Alumno on 19/03/24.
+//
+
+import SwiftUI
+
+struct TopicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopicView()
+}
---
+++ HistorIA/search_view.swift
@@ -0,0 +1,18 @@
+//
+//  search_view.swift
+//  HistorIA
+//
+//  Created by Alumno on 17/03/24.
+//
+
+import SwiftUI
+
+struct search_view: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    search_view()
+}
